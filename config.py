d19c7a06f36eb79cbb8ba074123fb10b6c7fa92b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-05 23:24:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
         """
 
         self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
+        self.datapath: str = os.path.join(os.getcwd(), 'data')
 
         # A S3 parameters template
         self.s3_parameters_template = 'https://raw.githubusercontent.com/data-intelligence-00003/.github/master/profile/s3_parameters.yaml'
