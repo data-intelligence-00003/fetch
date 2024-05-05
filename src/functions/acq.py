08e5a26803fd825fcbb55392b16d319a1a3757ad
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-05 22:30:13 +0100

    Add src/functions/acq.py

---
+++ src/functions/acq.py
@@ -0,0 +1,30 @@
+"""
+Module acq.py
+"""
+import requests
+
+class Acq:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def api(url: str) -> bytes:
+        """
+
+        :param url:
+        :return:
+        """
+
+        try:
+            response: requests.Response = requests.get(url=url, timeout=600)
+            response.raise_for_status()
+        except requests.exceptions.Timeout as err:
+            raise err from err
+        except Exception as err:
+            raise err from err
+
+        if response.status_code == 200:
+            content: bytes = response.content
+            return content
+        raise f'Failure code: {response.status_code}'
