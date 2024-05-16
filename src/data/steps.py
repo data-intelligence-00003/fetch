6062096afb18ee175652961a3aa48b58973c98d0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-16 23:54:00 +0100

    Update src/data/steps.py: In Progress

--- src/data/steps.py
+++ src/data/steps.py
@@ -1,13 +1,55 @@
+import os
+import pandas as pd
+
+import config
+import src.functions.streams
+import src.elements.text_attributes as txa
+import src.data.interface
+import src.elements.s3_parameters as s3p
+import src.elements.service as sr
 
 class Steps:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, service: sr.Service = None, s3_parameters: s3p.S3Parameters = None) -> None:
+        """
+        
+        :param service: A suite of services for interacting with Amazon Web Services.
+        :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
+                              name, buckets, etc.
+        """
+        
+        # For Amazon S3 & Amazon
+        self.__interface = src.data.interface.Interface(service=service, s3_parameters=s3_parameters)
+        
+        # Additionally
+        self.__configurations = config.Config()
+        self.__streams = src.functions.streams.Streams()
 
+    def __reference(self, name: str) -> pd.DataFrame:
+        """
+        
+        :param name: The name of a CSV data file within the project's data directory; including the file's extension.
+        :return:
+            A data frame.
+        """
 
-    def exc(self, hybrid: bool):
+        text = txa.TextAttributes(uri=os.path.join(self.__configurations.datapath, name), header=0)
+
+        return self.__streams.read(text=text)
+
+    def exc(self, hybrid: bool) -> list:
+        """
+        
+        :return:
+            A list
+        """
+
+        documents: pd.DataFrame = self.__reference(name=self.__configurations.documents)
+        organisations: pd.DataFrame = self.__reference(name=self.__configurations.organisations)
+        reference: pd.DataFrame = documents.merge(organisations, how='left', on='organisation_id').drop(columns=['organisation_type_id'])
+        dictionary = reference.to_dict(orient='records')
 
         if hybrid:
-            pass
+            return self.__interface.hybrid(dictionary=dictionary)
         else:
-            pass
+            return self.__interface.single(dictionary=dictionary)
