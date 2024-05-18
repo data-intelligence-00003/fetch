60ad29d56ceddecdcf46b44ebbd403f292fae743
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-18 19:48:46 +0100

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -96,9 +96,9 @@ class Interface:
         return f"Backup -> {state} ({metadata['organisation_name']}, {metadata['starting_year']})"
     
     @dask.delayed
-    def __matrix(self, url: str, buffer: bytes) -> pd.DataFrame:
+    def __matrix(self, url: str, buffer: bytes, metadata: dict) -> pd.DataFrame:
 
-        return self.__analytics.exc(url=url, buffer=buffer)
+        return self.__analytics.exc(url=url, buffer=buffer, metadata=metadata)
 
     def exc(self, dictionary: list[dict]):
 
@@ -108,12 +108,11 @@ class Interface:
         for metadata in dictionary[:4]:
             url: str = self.__url(metadata=metadata)
             buffer: bytes = self.__read(url=url)           
-            # cloud: str = self.__cloud(buffer=buffer, metadata=metadata)
-            # backup: str = self.__backup(buffer=buffer, metadata=metadata)
-            matrix: pd.DataFrame = self.__matrix(url=url, buffer=buffer)       
-            computations.append((matrix))
+            cloud: str = self.__cloud(buffer=buffer, metadata=metadata)
+            backup: str = self.__backup(buffer=buffer, metadata=metadata)
+            matrix: pd.DataFrame = self.__matrix(url=url, buffer=buffer, metadata=metadata)       
+            computations.append((cloud, backup, matrix))
 
         messages = dask.compute(computations)[0]
-        # messages = computations
         
         return messages
