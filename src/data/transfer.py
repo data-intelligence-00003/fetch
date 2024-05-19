a7e52559417fc9914df49f190c7002a55e407aae
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-19 01:51:58 +0100

    Update src/data/transfer.py

--- src/data/transfer.py
+++ src/data/transfer.py
@@ -10,6 +10,7 @@ import pandas as pd
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 import src.s3.ingress
+import config
 
 
 class Transfer:
@@ -26,13 +27,15 @@ class Transfer:
         """
         
         self.__reference: pd.DataFrame = reference
+        self.__configurations = config.Config()
 
         # The list of files to be uploaded
         self.__files: list[str] = glob.glob(
             pathname=os.path.join(self.__configurations.raw_, '**', '*.xlsx'), recursive=True)
 
         # An instance of Ingress
-        self.__ingress = src.s3.ingress.Ingress(service=service, s3_parameters=s3_parameters)
+        self.__s3_parameters = s3_parameters
+        self.__ingress = src.s3.ingress.Ingress(service=service, s3_parameters=self.__s3_parameters)
 
     def __tags(self) -> typing.Tuple[list[str], list[str], list[str]]:
         """
@@ -40,21 +43,21 @@ class Transfer:
         """
         
         names: list[str] =  [os.path.basename(file) for file in self.__files]        
-        years: list[str] = [os.path.basename(os.path.dirname(file)) for file in self.__files]
+        years: list[int] = [int(os.path.basename(os.path.dirname(file))) for file in self.__files]
         keys: list[str] = [f'{self.__s3_parameters.path_internal_raw}{year}/{name}' for year, name in zip(years, names)]
 
         return names, years, keys
     
-    def __dictionary(self, names: list[str], years: list[str]) -> list[dict]:
+    def __dictionary(self, names: list[str], years: list[int]) -> list[dict]:
         """
         
         :param names:
         :param years:
         """
 
-        identifiers = [pathlib.PurePath(name).stem for name in names]
+        identifiers: list[int] = [int(pathlib.PurePath(name).stem) for name in names]
         lines = pd.DataFrame(data={'organisation_id': identifiers, 'starting_year': years})
-        reference = self.__reference.copy().merge(lines, how='right', on=['organisation_id', 'starting_year'])
+        reference: pd.DataFrame = self.__reference.copy().merge(right=lines, how='right', on=['organisation_id', 'starting_year'])
         dictionary: list[dict] = reference.to_dict(orient='records')
 
         return dictionary
@@ -66,11 +69,11 @@ class Transfer:
         :return:
         """
 
-        ingress = dask.delayed(self.__ingress.exc)
-
         names, years, keys = self.__tags()
         dictionary = self.__dictionary(names=names, years=years)
 
+        # Compute
+        ingress = dask.delayed(self.__ingress.exc)
         computations = []
         for file, key, metadata in zip(self.__files, keys, dictionary):
             message = ingress(file=file, key=key, metadata=metadata)
