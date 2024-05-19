590b8b9d36ea3731156c3f218ff9d329d4e8d871
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-19 15:44:37 +0100

    Add src/data/reference.py

---
+++ src/data/reference.py
@@ -0,0 +1,50 @@
+"""Module reference.py"""
+import os
+
+import pandas as pd
+
+import config
+import src.elements.text_attributes as txa
+import src.functions.streams
+
+
+class Reference:
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.__configurations = config.Config()
+
+        # An instance for interacting with CSV files
+        self.__streams = src.functions.streams.Streams()
+        
+        # Get the inventory/metadata of climate documents & health organisations
+        documents: pd.DataFrame = self.__reference(name=self.__configurations.documents)
+        organisations: pd.DataFrame = self.__reference(name=self.__configurations.organisations)
+
+        # The frame of reference data
+        self.reference: pd.DataFrame = documents.merge(
+            right=organisations, how='left', on='organisation_id').drop(columns=['organisation_type_id'])
+
+    def __reference(self, name: str) -> pd.DataFrame:
+        """
+        
+        :param name: The name of a CSV reference file within the project's data directory; including the file's extension.
+        :return:
+            A data frame.
+        """
+
+        text = txa.TextAttributes(
+            uri=os.path.join(self.__configurations.datapath, name), header=0)
+
+        return self.__streams.read(text=text)
+
+    def __call__(self) -> pd.DataFrame:
+        """
+        :return:
+            A frame of references
+        """    
+
+        return self.reference
