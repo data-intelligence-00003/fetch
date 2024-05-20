72588d2d49ee3faf85b4083542b236812b0f1ada
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-20 13:39:00 +0100

    Add src/anomalies/emissions.py

---
+++ src/anomalies/emissions.py
@@ -0,0 +1,70 @@
+
+import pandas as pd
+
+import config
+import src.data.reference
+
+
+class Emissions:
+
+    def __init__(self) -> None:
+        """
+        Constructor
+        """
+        
+        self.__configurations = config.Config()
+        self.__reference = src.data.reference.Reference()
+
+    def __types(self, blob: pd.DataFrame) -> pd.DataFrame:
+        """
+        
+        :param blob:
+        :return: A frame
+        """
+
+        emission_types: pd.DataFrame = self.__reference.reader(name=self.__configurations.emission_types)
+
+        # Mapping strings
+        frame = blob.assign(mapping_string=blob['emission_type'].str.lower())
+        frame.drop(columns='emission_type', inplace=True)
+
+        # Missing mapping string
+        condition: pd.Series[bool] = frame['mapping_string'].isin(values=emission_types['mapping_string'].values)
+        frame.loc[~condition, 'mapping_string'] = 'other'
+
+        # Emission type identification codes
+        frame: pd.DataFrame = frame.copy().merge(right=emission_types, how='left', on='mapping_string')
+        
+        return frame.drop(columns='mapping_string')
+
+    def __sources(self, blob: pd.DataFrame) -> pd.DataFrame:
+        """
+        
+        :param blob:
+        :return: A frame
+        """
+
+        emission_sources: pd.DataFrame = self.__reference.reader(name=self.__configurations.emission_sources)
+
+        # Mapping strings
+        frame: pd.DataFrame = blob.assign(mapping_string=blob['emission_source'].str.lower())
+        frame.drop(columns='emission_source', inplace=True)
+
+        # Emission source identification codes
+        frame = frame.copy().merge(right=emission_sources, how='left', on=['emission_type_id', 'mapping_string'])
+        
+        return frame.drop(columns='mapping_string')
+    
+    def exc(self, blob: pd.DataFrame) -> pd.DataFrame:
+        """
+        
+        :param blob:
+        :return:
+            A data frame
+        """
+        
+        data: pd.DataFrame = blob.copy()
+        data = self.__types(blob=data)
+        data = self.__sources(blob=data)
+
+        return data
