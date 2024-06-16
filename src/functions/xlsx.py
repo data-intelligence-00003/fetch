19017a5eb6e7c5b5b5aa624c82c668a2fa774378
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-16 20:26:29 +0100

    Update src/functions/xlsx.py

--- src/functions/xlsx.py
+++ src/functions/xlsx.py
@@ -13,6 +13,7 @@ class XLSX:
     def __init__(self) -> None:
         pass
 
+    # noinspection PyTypeChecker
     @staticmethod
     def decode(buffer: bytes, sheet: src.elements.sheet.Sheet) -> pd.DataFrame:
         """
@@ -23,10 +24,12 @@ class XLSX:
 
         try:
             return pd.read_excel(io=io.BytesIO(initial_bytes=buffer), sheet_name=sheet.sheet_name,
-                                 header=sheet.header, skiprows=sheet.skiprows, nrows=sheet.nrows)
+                                 header=sheet.header, skiprows=sheet.skiprows, nrows=sheet.nrows,
+                                 usecols=sheet.usecols)
         except OSError as err:
             raise Exception(err.strerror) from err
 
+    # noinspection PyTypeChecker
     @staticmethod
     def read(sheet: src.elements.sheet.Sheet) -> pd.DataFrame:
         """
@@ -35,13 +38,14 @@ class XLSX:
         """
 
         try:
-            return pd.read_excel(
-                io=sheet.io, sheet_name=sheet.sheet_name, header=sheet.header,
-                skiprows=sheet.skiprows, nrows=sheet.nrows, engine='openpyxl')
+            return pd.read_excel(io=sheet.io, sheet_name=sheet.sheet_name, header=sheet.header,
+                                 skiprows=sheet.skiprows, nrows=sheet.nrows, usecols=sheet.usecols,
+                                 engine='openpyxl')
         except OSError as err:
             raise Exception(err.strerror) from err
 
-    def write(self, buffer: bytes, name: str) -> bool:
+    @staticmethod
+    def write(buffer: bytes, name: str) -> bool:
         """
         
         :param buffer:
